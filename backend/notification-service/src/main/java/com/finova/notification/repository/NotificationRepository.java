76452d3a5d457325ec4a3e46758ca5bef9a8b2b4
abrar2030 <abrarahmedpei@gmail.com>
2024-11-11 11:52:05 +0100

    Auto-commit on 2024-11-11 11:48:27

--- backend/notification-service/src/main/java/com/finova/notification/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.finova.notification.repository;
-
-import com.finova.notification.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
